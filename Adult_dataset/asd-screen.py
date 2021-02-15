5213a0eb598a41f5b24873f842035bf5cabb2229
Blessin Varkey <blessinvarkey@gmail.com>
2021-02-16 00:43:04 +0530

    Update asd-screen.py

--- Adult_dataset/asd-screen.py
+++ Adult_dataset/asd-screen.py
@@ -8,7 +8,27 @@ import unittest
 
 class ASDScreening():
 
-#def read_file(self, file_name, sample=True):
+def read_file(self, file_name, sample=True):
 with open("Autism_Data.arff", 'r') as file:
     df = pd.read_csv(file)
     print(df)
+
+    def oneHotEncoding():
+        pass
+
+    def train_test_split(seld, df, train_frac = 0.7, seed=1):
+        df_matrix = df.values
+        np.random.seed(seed)
+        np.random.shuffle(df_matrix) #shuffle the data
+        train_size = int(df_matrix.shape[0]*train_frac) #train the data
+        train_features = def_matrix[:train_size, :-1] #except last column
+        train_labels = df_matrix[:train_size, -1] #last column
+        test_features = df_matrix[train_size:, :-1] #test data
+        test_labels = df_matrix[train_size:, -1]
+
+        return (train_features, train_labels), (test_features, test_labels)
+
+
+
+class TestASDScreening(unittest.TestCase):
+   pass
